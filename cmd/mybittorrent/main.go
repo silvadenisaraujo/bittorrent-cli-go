16ecd5d320e8a0b15df123c21356b61a7b54fa10
Dênis Araújo da Silva <silvadenisaraujo@gmail.com>
2023-09-17 13:42:39 +0200

    Add decode dictionary

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -12,9 +12,12 @@ import (
 func decodeBencodeString(bencodedString string) (interface{}, int, error) {
 	var stringLenNumberLenght int
 
+	// fmt.Println("To decode string=", bencodedString)
+
 	for i, char := range bencodedString {
 		if char == ':' {
 			stringLenNumberLenght = i
+			break
 		}
 	}
 
@@ -30,6 +33,8 @@ func decodeBencodeString(bencodedString string) (interface{}, int, error) {
 func decodeBencodeInteger(bencodedString string) (interface{}, int, error) {
 	bencodedStringLen := len(bencodedString)
 	var integerLen int
+
+	// fmt.Println("To decode integer=", bencodedString)
 
 	for i := 1; i < bencodedStringLen; i++ {
 		if bencodedString[i] == 'e' {
@@ -53,6 +58,8 @@ func decodeBencodeList(bencodedString string) (interface{}, int, error) {
 	var decodedList []interface{} = make([]interface{}, 0)
 	var listLen int = len(bencodedString)
 
+	// fmt.Println("To decode list=", bencodedString)
+
 	// Remove the first (l) and last character (e)
 	bencodedPayload := bencodedString[1 : listLen-1]
 
@@ -68,6 +75,39 @@ func decodeBencodeList(bencodedString string) (interface{}, int, error) {
 	return decodedList, listLen, nil
 }
 
+// Decodes a Bencode dictionary
+func decodeBencodeDictionary(bencodedString string) (interface{}, int, error) {
+	var decodedDictionary map[string]interface{} = make(map[string]interface{})
+	var dictionaryLen int = len(bencodedString)
+
+	// Remove the first (d) and last character (e)
+	bencodedPayload := bencodedString[1 : dictionaryLen-1]
+
+	for len(bencodedPayload) > 0 {
+
+		fmt.Println("To decode=", bencodedPayload)
+
+		decodedKey, keyEnd, err := decodeBencode(bencodedPayload)
+		if err != nil {
+			return "", -1, err
+		}
+
+		fmt.Println("Decoded key=", decodedKey, "keyEnd=", keyEnd)
+
+		decodedValue, valueEnd, err := decodeBencode(bencodedPayload[keyEnd:])
+		if err != nil {
+			return "", -1, err
+		}
+
+		fmt.Println("Decoded value=", decodedValue, "valueEnd=", valueEnd)
+
+		decodedDictionary[decodedKey.(string)] = decodedValue
+		bencodedPayload = bencodedPayload[keyEnd+valueEnd:]
+	}
+
+	return decodedDictionary, dictionaryLen, nil
+}
+
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
@@ -78,6 +118,8 @@ func decodeBencode(bencodedString string) (interface{}, int, error) {
 		return decodeBencodeInteger(bencodedString)
 	} else if bencodedString[0] == 'l' {
 		return decodeBencodeList(bencodedString)
+	} else if bencodedString[0] == 'd' {
+		return decodeBencodeDictionary(bencodedString)
 	} else {
 		return "", -1, fmt.Errorf("Pattern not recognized %s", bencodedString)
 	}
